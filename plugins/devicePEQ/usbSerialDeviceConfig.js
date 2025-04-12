ca3dabf574c1b4f351e165d80173018712330392
Mark Ryan Sallee <markryansallee@gmail.com>
2025-04-12 10:57:59 -0700

    Imported devicePEQ plugin
    Error on pushing pregain value

---
+++ plugins/devicePEQ/usbSerialDeviceConfig.js
@@ -0,0 +1,25 @@
+// Dynamically import the USB Serial handler
+const { jdsLabsUsbSerial } = await import('./jdsLabsUsbSerialHandler.js');
+
+export const usbSerialDeviceHandlerConfig = [
+  {
+    vendorId: 0x152a, // JDS Labs USB Vendor ID (common for JDS Labs / Teensy based boards)
+    manufacturer: "JDS Labs",
+    handler: jdsLabsUsbSerial,
+    devices: {
+      "Element IV": {
+        usbProductId: 35066,
+        modelConfig: {
+          minGain: -12,
+          maxGain: 12,
+          maxFilters: 10,
+          firstWritableEQSlot: 0,
+          maxWritableEQSlots: 1,
+          disconnectOnSave: false,
+          disabledPresetId: -1,
+          availableSlots: [{ id: 0, name: "Headphones" },{ id: 1, name: "RCA" }]
+        }
+      }
+    }
+  }
+];
